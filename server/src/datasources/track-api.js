457c66046af225e532c11ca74c018c6bc60aeb2d
echizenyayota <virtualmisso30@gmail.com>
2022-01-21 16:54:10 +0900

    Resolvers chains

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -17,7 +17,11 @@ class TrackAPI extends RESTDataSource {
 
   getTrack(trackId) {
     return this.get(`track/${trackId}`);
-  } 
+  }
+
+  getTrackModules(trackId) {
+    return this.get(`track/${trackId}/modules`);
+  }
 
 
 }
