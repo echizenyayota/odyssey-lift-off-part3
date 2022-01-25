66b196a783b07c5cc0bc0211306a1422c479dc7f
echizenyayota <virtualmisso30@gmail.com>
2022-01-25 20:02:31 +0900

    Building the track page (fixed)

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -2,28 +2,28 @@ import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
 
-const GET_TRACK = gql`
-query getTrack($trackId: ID!) {
-  track(id: $trackId) {
-    id
-    title
-    author {
-      id
-      name
-      photo
-    }
-    thumbnail
-    length
-    modulesCount
-    numberOfViews
-    modules {
+export const GET_TRACK = gql`
+  query getTrack($trackId: ID!) {
+    track(id: $trackId) {
       id
       title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
       length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
     }
-    description
   }
-}
 
 
 `;
